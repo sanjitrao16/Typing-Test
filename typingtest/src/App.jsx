2066439662800baab71ed9b2ded3daf8778f6298
sanjitrao16 <sanjitrao1605@gmail.com>
2025-05-15 19:50:08 +0530

    dummy change

--- typingtest/src/App.jsx
+++ typingtest/src/App.jsx
@@ -5,7 +5,13 @@ import viteLogo from "/vite.svg";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
