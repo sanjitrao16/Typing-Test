718a1442523e5c60a86b16b770a32b7e6366ff15
Sanjit Rao <152643690+sanjitrao16@users.noreply.github.com>
2025-12-11 17:30:26 +0530

    Change NavBar link href from '/' to '#'

--- typingtest/src/components/NavBar.jsx
+++ typingtest/src/components/NavBar.jsx
@@ -9,7 +9,7 @@ function NavBar() {
         </div>
         <div className="bg-[#60ff4424] py-2 px-3 rounded-2xl">
           <a
-            href="/"
+            href="#"
             className="flex gap-2 text-[#39ff14] hover:text-[#76cc67] transition-colors"
           >
             <img
